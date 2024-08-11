783bd167e38f87d6eed706cac4272370c32238ea
Emre Caglar Hosgor <emrecaglarhosgor@gmail.com>
2024-08-11 07:18:47 +0300

    powershell hash calculation v0.1

---
+++ powershell/hash_calculator_v0.1.ps1
@@ -0,0 +1,21 @@
+﻿$path_ = Read-Host "Please enter the file path for hash calculation"
+
+Function Get-HashType {
+    $type=Read-Host "
+        1 - MD5
+        2 - SHA1
+        3 - SHA256
+        4 - SHA512
+        Please choose "
+    Switch ($type) {
+        1 {$choice="MD5"}
+        2 {$choice="SHA1"}
+        3 {$choice="SHA256"}
+        4 {$choice="SHA512"}    
+    }
+    return $choice
+}
+
+$sha_ = Get-HashType
+
+get-fileHash -Algorithm $sha_ $path_| Format-List
