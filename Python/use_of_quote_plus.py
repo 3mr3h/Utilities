82e0a980544fd989c58d3d72357bd6dab5c2bb4c
Emre Caglar Hosgor <emrecaglarhosgor@gmail.com>
2024-10-16 22:53:21 +0200

    update repo

--- Python/use_of_quote_plus.py
+++ Python/use_of_quote_plus.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct 13 20:52:37 2024
+Created on Sun Oct 13
 
-@author: emrec
+@author: emre hosgor
 Use of quote_plus
+Small tasks
 """
 
 from urllib.parse import quote_plus
 
 # Example parameters, change them
 params = {
-    "name": "John Doe",
+    "name": "Emre Hosgor",
     "city": "New York",
-    "query": "ice cream & chocolate"
+    "query": "Admin & admin"
 }
 
 # Encode each parameter
 encoded_params = "&".join([f"{key}={quote_plus(value)}" for key, value in params.items()])
 
 # Construct URL
-url = f"https://www.example.com/search?{encoded_params}"
+url_ = "https://www.example.com/search?"
+url = f"{url_}{encoded_params}"
 
 print(url)
